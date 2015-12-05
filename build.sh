6665b7f5060ecd76641f12dc90937649f897b3e5
Kenjiro Nakayama <nakayamakenjiro@gmail.com>
2015-12-05 21:31:21 +0900

    Remove restriction of Linux build only

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ APP_NAME="kubernetes-manifest-scanner"
 ORG_PATH="github.com/nak3"
 REPO_PATH="${ORG_PATH}/${APP_NAME}"
 
-GOBIN=${PWD}/bin
 GOPATH=${PWD}/Godeps/_workspace:${PWD}/_output/local/go/
 
 if [ ! -h _output/local/go/src/${REPO_PATH} ]; then
@@ -12,13 +11,8 @@ if [ ! -h _output/local/go/src/${REPO_PATH} ]; then
   ln -s ../../../../../.. _output/local/go/src/${REPO_PATH}
 fi
 
-eval $(go env)
+echo "Building ${APP_NAME}..."
+go build -o ${PWD}/bin/${APP_NAME} ${PWD}/_output/local/go/src/github.com/nak3/kubernetes-manifest-scanner/pkg/main.go
 
-if [ ${GOOS} = "linux" ]; then
-  echo "Building ${APP_NAME}..."
-  go build -o ${GOBIN}/${APP_NAME} ${PWD}/_output/local/go/src/github.com/nak3/kubernetes-manifest-scanner/pkg/main.go
-  echo "Building bash_comp..."
-  go build -o ${GOBIN}/bash_comp_autogenerater ${PWD}/_output/local/go/src/github.com/nak3/kubernetes-manifest-scanner/pkg/gen_bash_comp.go
-else
-  echo "Not on Linux - skipping ${APP_NAME} build"
-fi
+echo "Building bash_comp_autogenerator..."
+go build -o ${PWD}/bin/bash_comp_autogenerator ${PWD}/_output/local/go/src/github.com/nak3/kubernetes-manifest-scanner/pkg/gen_bash_comp.go
--- godeprun.sh
+++ godeprun.sh
@@ -4,26 +4,22 @@ APP_NAME=kubernetes-manifest-scanner
 ORG_PATH="github.com/nak3"
 REPO_PATH="${ORG_PATH}/kubernetes-manifest-scanner"
 
-export GOBIN=${PWD}/bin
 export GOPATH=${PWD}/gopath
+
+if ! type godep > /dev/null 2>&1; then
+  echo "You haven't installed godep... Please install first."
+  echo "go get github.com/tools/godep"
+fi
 
 if [ ! -h gopath/src/${REPO_PATH} ]; then
   mkdir -p gopath/src/${ORG_PATH}
   ln -s ../../../.. gopath/src/${REPO_PATH} || exit 255
 fi
 
-eval $(go env)
-
-if [ ${GOOS} = "linux" ]; then
-  echo "Getting dependencies..."
-  go get github.com/tools/godep
-  go get github.com/nak3/jvmap
-  go get github.com/spf13/cobra
-  go get k8s.io/kubernetes/pkg/kubectl/cmd/util
-else
-  echo "Not on Linux - skipping $APP_NAME build"
-fi
-
+echo "Getting dependencies into ${GOPATH}..."
+go get github.com/nak3/jvmap
+go get github.com/spf13/cobra
+go get k8s.io/kubernetes/pkg/kubectl/cmd/util
 
 if [ ! -e '.git' ]; then
   echo '.git directory not found'
@@ -31,7 +27,5 @@ if [ ! -e '.git' ]; then
   exit 1
 fi
 
-# echo "Making git repository..."
-#
 echo "Running godep save..."
-${GOBIN}/godep save github.com/nak3/kubernetes-manifest-scanner/pkg
+godep save github.com/nak3/kubernetes-manifest-scanner/pkg
